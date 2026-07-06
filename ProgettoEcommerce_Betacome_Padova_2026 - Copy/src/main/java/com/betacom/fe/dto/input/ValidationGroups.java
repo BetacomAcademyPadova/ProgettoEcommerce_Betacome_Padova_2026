a75a8ac34a3f991f888488ede88c2c573bc2b442
Vadim <49763091+Vrudim@users.noreply.github.com>
2026-07-06 15:20:58 +0200

    Delete ProgettoEcommerce_Betacome_Padova_2026 - Copy directory

--- ProgettoEcommerce_Betacome_Padova_2026 - Copy/src/main/java/com/betacom/fe/dto/input/ValidationGroups.java
+++
@@ -1,7 +0,0 @@
-package com.betacom.fe.dto.input;
-
-public interface ValidationGroups {
-	interface Create {}
-
-	interface Update {}
-}
--- ProgettoEcommerce_Betacome_Padova_2026 - Copy/src/main/java/com/betacom/fe/services/interfaces/IMessaggioServices.java
+++
@@ -1,5 +0,0 @@
-package com.betacom.fe.services.interfaces;
-
-public interface IMessaggioServices {
-	String get(String code);
-}
