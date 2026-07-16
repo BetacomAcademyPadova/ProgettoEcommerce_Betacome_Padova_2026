41d2f402d70a0f1351eaaa8862f0c6a492a3d12b
garatomarco <garatoma@gmail.com>
2026-07-16 17:45:51 +0200

    Aggiunti test ProdottiOrdine e Ricevuta

---
+++ ProgettoEcommerce_Betacome_Padova_2026/src/test/java/com/betacom/fe/tipo/ProdottiOrdineTest.java
@@ -0,0 +1,60 @@
+package com.betacom.fe.tipo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.betacom.fe.dto.input.ProdottiOrdineReq;
+import com.betacom.fe.repositories.IProdottiCarrelloRepository;
+
+import lombok.extern.slf4j.Slf4j;
+import tools.jackson.databind.ObjectMapper;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ProdottiOrdineTest {
+	
+	@Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    
+    @Autowired
+    private IProdottiCarrelloRepository procarR;
+    
+    @Test
+    @Order(1)
+    public void createProdottiOrdineTest() throws Exception {
+
+        Integer idCarrello = procarR.findAll()
+                .get(0)
+                .getIdRiga();
+
+        ProdottiOrdineReq req = new ProdottiOrdineReq();
+
+        req.setOrdineId(1);
+        req.setProdottoId(2);
+        req.setIndirizzoSpedizioneId(3);
+        req.setProdottiCarrelloId(idCarrello);
+        req.setDivisioneOrdineId(2);
+
+
+        mockMvc.perform(post("/rest/ProdottiOrdine/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(req)))
+                .andExpect(status().isOk());
+    }
+
+}
