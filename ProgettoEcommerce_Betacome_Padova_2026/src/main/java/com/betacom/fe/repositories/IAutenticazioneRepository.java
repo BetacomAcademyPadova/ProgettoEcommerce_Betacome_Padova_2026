adcc257c1652eb7a805514baca2f8d8707631d29
Vrudim <v.musteata96@gmail.com>
2026-07-17 15:50:10 +0200

    Merge branch 'main' of git@github.com:Vrudim/ProgettoEcommerce_Betacome_Padova_2026.git into Vadim

--- ProgettoEcommerce_Betacome_Padova_2026/src/main/java/com/betacom/fe/repositories/IAutenticazioneRepository.java
+++ ProgettoEcommerce_Betacome_Padova_2026/src/main/java/com/betacom/fe/repositories/IAutenticazioneRepository.java
@@ -8,6 +8,5 @@ import com.betacom.fe.models.Autenticazione;
 
 public interface IAutenticazioneRepository extends JpaRepository<Autenticazione, Integer>{
 	Optional<Autenticazione> findByUserUserId(Integer userId);
-
-	Optional<Autenticazione> findByUsername(String username);
+	Optional<Autenticazione> findByUsername(String usr);
 }
